cd5d8b95960b967091593ed61b49190ce1f5f259
Margarita Medvedeva <margaret_ne@rambler.ru>
2020-02-13 15:38:05 +0300

    HW_1

--- src/Less_1/Apple.java
+++ src/Less_1/Apple.java
@@ -4,5 +4,10 @@ package Less_1;
  * Created by mma on 12.02.2020.
  */
 public class Apple extends Fruit{
+    private float weight = 1.0f;
 
+    @Override
+    public float getWeight() {
+        return weight;
+    }
 }
