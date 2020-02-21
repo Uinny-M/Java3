47741725534b75860ac54c1caaeb13a91e755294
Margarita Medvedeva <margaret_ne@rambler.ru>
2020-02-21 16:52:31 +0300

    HW_1 - 01

---
+++ src/Less_2/PrintStream.java
@@ -0,0 +1,6 @@
+package Less_2;
+public class PrintStream {
+    public static void main(String[] args) {
+        System.out.printf("%6d%n%6d%n%6d%n%6d%n%6d%n%6d", 666666, 55555, 4444, 333, 22, 1);
+    }
+}
